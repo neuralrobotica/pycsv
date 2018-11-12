44d0d215cb0268cfd04d0f6f4c2382e2ea44aeef
Ricardo de Jong <neural@h2808141.stratoserver.net>
2018-11-12 18:52:37 +0100

    pycsv code

---
+++ reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3 
+#
+# python csv reader, for testing if a csv file can work with this module.
+#
+#
+# create a function that can modfiy a csv file, or read historical data from Metatrader 5 market data.
+#
+
+def function():
+	print("This is a test funcion")
+
+
+if __name__ == "__main__":
+	main()
+
+
