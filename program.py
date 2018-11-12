cbffafa18a55c73010b2ac392b978c7dbe38e716
Ricardo de Jong <neural@h2808141.stratoserver.net>
2018-11-12 21:08:14 +0100

    src program.py

--- program.py
+++ program.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 #
 # program for testing csv file reading
+
+import  csv
+
+data = csv.reader(open('data.csv', newline=''), delimiter=' ', quotechar='|')
+
+for row in data:
+	print (','.join(row))
+
+
+'''
+with open('data.csv', newline='') as f:
+	reader = csv.reader(f)
+	for row in reader:
+		print(row)
+
+'''
+'''
+with open('data.csv', newline='') as f:
+	reader = csv.reader(f, delimiter=':', quoting=csv.QUOTE_NONE)
+	for row in reader:
+		print(row)
+'''
